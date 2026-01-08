f9be2ec6076bdee9564905922b214df3a0077c78
JulioLeitee <JulioLeitee>
2026-01-08 20:32:34 -0300

    atividade prática - questão 03

---
+++ Atividade Prática 01/questão3.py
@@ -0,0 +1,22 @@
+comprimento = 12
+largura = 14
+altura = 20
+
+
+Volume = comprimento * largura * altura
+print("O volume é:", Volume, "cm³")
+
+
+
+
+
+
+
+
+
+
+'''
+Comprimento: 12 cm
+Largura: 14 cm
+Altura: 20 cm
+'''
