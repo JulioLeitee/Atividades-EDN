d95c18a236215a8d2f18ae72be47be313dab410a
JulioLeitee <JulioLeitee>
2026-01-09 20:33:47 -0300

    Atividade 02 - Questão 03

---
+++ Atividade Prática 02/3- Calculadora de Média Escolar.py
@@ -0,0 +1,10 @@
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+nota3 = float(input("Digite a terceira nota: "))
+
+media = (nota1 + nota2 + nota3) / 3
+
+print(f"Nota 1: {nota1}")
+print(f"Nota 2: {nota2}")
+print(f"Nota 3: {nota3}")
+print(f"Média: {media:.2f}")
