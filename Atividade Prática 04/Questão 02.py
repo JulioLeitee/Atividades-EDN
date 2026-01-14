07dcdc9aaf3bad2bc126abb88ea533c3f0a6fbde
JulioLeitee <JulioLeitee>
2026-01-14 19:49:06 -0300

    Atividaded Prática 04 - Questão 02

---
+++ Atividade Prática 04/Questão 02.py
@@ -0,0 +1,28 @@
+notas = []
+
+while True:
+    try:
+        entrada = input("Digite uma nota ( 0 a 10) ou 'fim' para sair: ")
+
+        if entrada.lower() == 'fim':
+            break
+
+        nota = float(entrada)
+        if nota < 0 or nota > 10:
+            raise Exception()
+        notas.append(nota)
+
+    except ValueError:
+        print("Você deve digitar apenas números")
+    except Exception:
+        print("Nota Inválida")
+
+    soma = 0 
+
+    for nota in notas:
+        soma += nota
+    
+media = soma / len(notas)
+
+print(f"Média Final:  {media:.2f}")
+
