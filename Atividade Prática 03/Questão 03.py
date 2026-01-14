a483406f4ae33a902139ca876db6d8fbfaed64e7
JulioLeitee <JulioLeitee>
2026-01-14 19:19:08 -0300

    Atividade prática 03 - Questão 03

--- Atividade Prática 03/Questão 03.py
+++ Atividade Prática 03/Questão 03.py
@@ -1,18 +1,27 @@
+temperatura = float(input("Digite a temperatura: "))
+origem = input("Digite a unidade de origem (C, F, K): ").upper()
+destino = input("Digite a unidade de destino (C, F, K): ").upper()
 
 
+if origem == destino:
+    resultado = temperatura
+elif origem == "C":
+    if destino == "F":
+        resultado = (temperatura * 9/5) + 32
+    elif destino == "K":
+        resultado = temperatura + 273.15
+
+elif origem == "F":
+    if destino == "C":
+        resultado = (temperatura - 32) * 5/9
+    else: 
+        resultado = (temperatura - 32) * 5/9 + 273.15
+
+else:
+    if destino == "C":
+        resultado = temperatura - 273.15
+    else:
+        resultado = (temperatura - 273.15) * 9/5 + 32
 
 
-
-
-
-
-
-
-
-
-
-
-'''3- Conversor de Temperatura
-Crie um programa que converta temperaturas entre Celsius, Fahrenheit e Kelvin.
-O usuário deve informar a temperatura, a unidade de origem e a unidade para qual deseja converter.
-'''
+print(f"{temperatura}°{origem} é igual a {resultado:.2f}°{destino}")
