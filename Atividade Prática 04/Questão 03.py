641db65d5fa53677fb4f63adebd83a04381cf60f
JulioLeitee <JulioLeitee>
2026-01-14 20:15:47 -0300

    Atividade Prática 04 - Questão 03

---
+++ Atividade Prática 04/Questão 03.py
@@ -0,0 +1,24 @@
+while True:
+    try:
+        senha = input("Digite a senha: ")
+
+        if senha.lower() == 'sair':
+            print("Saindo do programa...")
+            break
+
+        if len(senha) < 8:
+            raise Exception("A Senha deve ter pelo menos 8 caracteres")
+
+        tem_numero = False
+        for caractere in senha:
+            if caractere.isdigit():
+                tem_numero = True
+                break
+        
+        if tem_numero == False:
+            raise Exception("A Senha deve conter pelo menos um número")
+        
+        print("Senha Válida")
+               
+    except Exception as e:
+        print(f"Erro: {e} Tente novamente.")
